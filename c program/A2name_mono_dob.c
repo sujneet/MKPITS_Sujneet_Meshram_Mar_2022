af2b40deb3049d1063752c06b6b1881e69867bf9
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-05 21:48:47 +0530

    A2 name mo no DOB

---
+++ c program/A2name_mono_dob.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int dob,mobile_number;
+	char name;
+	printf("Name  :Alexandra Abramov\n");
+	printf("DOB   :July 14, 1975\n");
+	printf("Mobile:99-99999999\n");
+	}
