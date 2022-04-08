a67a86b45f1fe08657484eeef240a3cf8ccdd6f7
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-08 09:29:10 +0530

    A28 average of positive number

---
+++ c program/A28 avg of positive numbers.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+	int a1,a2,a3,a4,a5,positive=0,negative=0,total,avg;
+	printf("enter 1 numbers");
+	scanf("%d",&a1);	
+	printf("enter 2 numbers");
+	scanf("%d",&a2);	
+	printf("enter 3 numbers");
+	scanf("%d",&a3);	
+	printf("enter 4 numbers");
+	scanf("%d",&a4);	
+	printf("enter 5 numbers");
+	scanf("%d",&a5);
+	if(a1>0)
+	{
+		positive=positive+1;
+		total +=a1;
+	}
+	else
+	{
+		negative=negative+1;
+	}
+	if(a2>0)
+	{
+		positive=positive+1;
+		total +=a2;
+	}
+	else
+	{
+		negative=negative+1;
+	}
+	if(a3>0)
+	{
+		positive=positive+1;
+		total +=a3;
+	}
+	else
+	{
+		negative=negative+1;
+	}
+	if(a4>0)
+	{
+		positive=positive+1;
+		total+=a4;
+	}
+	else
+	{
+		negative=negative+1;
+	}
+	if(a5>0)
+	{
+		positive=positive+1;
+		total+=a5;
+	}
+	else
+	{
+		negative=negative+1;
+	}
+	avg=total/positive;
+	printf("positive numbers are %d\n",positive);
+	printf("average of positive number are %d",avg);
+	
+}
