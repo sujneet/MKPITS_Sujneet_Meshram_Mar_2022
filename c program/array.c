fba1f1038e065dd56c1a4a8754d15e656ce1d82e
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-21 17:25:06 +0530

    Array

---
+++ c program/array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a[3];
+	a[0]=12;
+	a[1]=155;
+	a[2]=1200;
+	a[3]=1244;
+	printf("%d\n",a[0]);
+	printf("%d\n",a[1]);
+	printf("%d\n",a[2]);
+	printf("%d\n",a[3]);
+}
