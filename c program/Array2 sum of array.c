bb457d5e167cbdbe831b99f25f663e02e03fb025
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-22 12:57:13 +0530

    Array2 sum of array

---
+++ c program/Array2 sum of array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a[4],sum=0,i;
+	printf("enter 5 element");
+	scanf("%d\n",a[i]);
+
+	for(i=0;i<=a[i];i++)
+	{
+		
+		sum=sum+a[i];
+	}
+	printf("sum of array is %d",sum);
+}
