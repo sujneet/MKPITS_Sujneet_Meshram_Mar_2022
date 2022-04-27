1f35ebd102e13b18d6c97822b81ce0dc9920ba60
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-27 14:01:26 +0530

    A48 smallest element of array

---
+++ c program/A48 smallest element of array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[6];
+	int i,t,p;
+
+	for(i=0; i<6; i++) 
+	{
+    	printf("Enter Number : ");
+		scanf("%d", &a[i]);
+	}
+	t = a[0];
+	p = 0;
+
+	for(i=0; i<6; i++) 
+	{
+		if(a[i]<t) 
+		{
+			t= a[i];
+			p = i;
+		}
+	}
+
+	printf("Smallest Value: %d\n", t);
+	printf(" a[%d] = %d\n",a[t],t);
+}
