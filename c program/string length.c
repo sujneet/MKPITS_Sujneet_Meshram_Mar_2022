f8e5208b9b3708d4e979448a6ba2ccdb43a8a184
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-27 14:16:44 +0530

    string length

---
+++ c program/string length.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char name[10];
+	printf("enter string : ");
+	scanf("%s",&name);
+	printf("\n name length %d",strlen(name));
+}
