891d7dc9e7d7522ff6400db2b134f1b5c76f3ba1
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-28 13:03:19 +0530

    string reverse without using string function

---
+++ c program/String Reverse Without Using Str Fun.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char name[10];
+
+	int i,count=0;
+
+	printf("Enter any Name: ");
+	scanf("%s",name);
+
+	for(i=0; name[i]!='\0';i++)
+	{
+		count=count+1;
+	}
+
+    for(i=count-1;i>=0;i--)
+    {
+       printf("%c",name[i]);
+    }
+
+}
