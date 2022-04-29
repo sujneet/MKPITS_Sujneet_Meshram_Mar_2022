59977928b287cbfd35a92d943ae195b799e3f9d5
Sujneet <sujneetmeshram2018@gmail.com>
2022-04-29 10:28:21 +0530

    A73

---
+++ c program/A73 multiplication of three number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter three number for multiplication");
+	scanf("%d %d %d",&a,&b,&c);
+	d=a*b*c;
+	printf("multiplication of three number is %d",d);
+}
