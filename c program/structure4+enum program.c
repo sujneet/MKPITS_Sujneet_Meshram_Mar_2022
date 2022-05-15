16eea6436a2473c46d2ae6589444ae359cd1b12e
Sujneet <sujneetmeshram2018@gmail.com>
2022-05-15 13:53:22 +0530

    structure4+enum

---
+++ c program/structure4+enum program.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+struct customer
+{
+	char name[30];
+	int id;
+	int mono[10];	
+};
+void main()
+{
+	int i;
+	struct customer s[5];
+	printf("customer name,id,mo no");
+	for(i=0;i<=5;i++)
+	{
+		printf("enter name");
+		scanf("%s",&s[i].first name);
+		printf("enter id");
+		scanf("%d",&s[i].id);
+		printf("enter mo no");
+		scanf("%d",&s[i].mono);
+	}
+	
+}
