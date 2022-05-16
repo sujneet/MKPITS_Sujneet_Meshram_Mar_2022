b9621a3b1b6c8c8322bb9992cb461278ab47a2c4
Sujneet <sujneetmeshram2018@gmail.com>
2022-05-16 10:28:05 +0530

    A121

---
+++ c program/A121 check if a given array of integers contains 5 next to a 5 somewhere.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,t=0;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the array element : \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        if (a[i]==5&&a[i]==a[i + 1]) 
+        {
+            t=1;
+            break;
+        }
+    }
+    
+    if(t==1)
+    {
+        printf("TRUE");
+    }
+    else
+    {
+        printf("FALSE");
+    }
+}
