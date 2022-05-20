7373dfc1cee3aeace8ed8d5f90695da26f00cd86
Sujneet <sujneetmeshram2018@gmail.com>
2022-05-20 11:52:45 +0530

    A146

---
+++ c program/A146 display rectangle of number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter A Number You Want to Print : ");
+	scanf("%d",&n);
+	printf("\n \n ");
+	printf("Rectangle Of 3 Column and 5 Row : \n\n");
+	printf(" %d %d %d \n",n,n,n);
+	printf(" %d   %d \n",n,n);
+	printf(" %d   %d \n",n,n);
+	printf(" %d %d %d \n",n,n,n);
+}
