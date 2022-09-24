c002cdaab01a9df3a03d42636f538b8d7093d551
Sujneet <sujneetmeshram2018@gmail.com>
2022-09-24 09:39:07 +0530

    1 normalization

---
+++ SQL/1NF - Copy.sql
@@ -0,0 +1,6 @@
+create table Employee(emp_id int ,emp_name varchar(40) not null,emp_phone int,emp_state varchar(40) not null)
+
+insert into Employee  values(2,'Amit',24546688,'Maha'),(4,'Arjun',87945568,'Mp'),(5,'Rutuja',84975615,'Maha'),
+(9,'Priya',45976512,'Mp')
+
+select*from Employee
